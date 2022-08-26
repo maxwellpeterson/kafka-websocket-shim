d6650a10f778705ad1584c6fff8b3589ae222a62
Max Peterson <maxpeterson00@gmail.com>
2022-08-25 18:32:35 -0700

    Switch from sarama to franz-go

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"os"
 
-	"github.com/Shopify/sarama"
 	"github.com/maxwellpeterson/kafka-websocket-shim/shim"
 	"github.com/pkg/errors"
+	"github.com/twmb/franz-go/pkg/kgo"
+	"github.com/twmb/franz-go/pkg/kversion"
 )
 
 var broker = flag.String("broker", "", "the hostname plus port of the broker, such as localhost:8787")
@@ -19,17 +21,29 @@ func main() {
 		log.Fatal("broker flag required")
 	}
 
-	sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
+	dialer := shim.NewDialer(shim.DialerConfig{Tls: *tls})
+	logger := kgo.BasicLogger(
+		os.Stdout,
+		kgo.LogLevelDebug,
+		func() string { return "[kgo] " },
+	)
 
-	cfg := sarama.NewConfig()
-	cfg.Net.Proxy.Enable = true
-	cfg.Net.Proxy.Dialer = shim.NewDialer(shim.DialerConfig{Tls: *tls})
-	cfg.Version = sarama.V0_8_2_0
-
-	_, err := sarama.NewClient([]string{*broker}, cfg)
+	cl, err := kgo.NewClient(
+		kgo.SeedBrokers(*broker),
+		kgo.MaxVersions(kversion.V0_8_0()),
+		kgo.ConsumeTopics("test-topic"),
+		kgo.Dialer(dialer.DialContext),
+		kgo.WithLogger(logger),
+	)
 	if err != nil {
-		log.Fatal(errors.Wrap(err, "create client failed"))
+		log.Fatal(errors.Wrap(err, "client create failed"))
 	}
-
+	defer cl.Close()
 	log.Println("Created client!")
+
+	record := &kgo.Record{Topic: "test-topic", Value: []byte("bar")}
+	if err := cl.ProduceSync(context.Background(), record).FirstErr(); err != nil {
+		log.Fatal(errors.Wrap(err, "produce failed"))
+	}
+	log.Println("Produced record!")
 }
